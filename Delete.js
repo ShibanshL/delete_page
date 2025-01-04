933a4754cd0abfe9e8ccf26758bd4d59bbd7b79a
Shibansh Sahoo <81880059+ShibanshL@users.noreply.github.com>
2025-01-04 21:37:23 +0530

    first commit

---
+++ Delete.js
@@ -0,0 +1,11 @@
+const github = document.querySelector("body > div > div > div.IconL")
+
+const linkedin = document.querySelector("body > div > div > div.IconG")
+
+github.addEventListener('click', () => {
+    window.open('https://github.com/ShibanshL');
+})
+
+linkedin.addEventListener('click', () => {
+    window.open('https://www.linkedin.com/in/shibansh-sahoo-a436061a3/', '_blank');
+})
